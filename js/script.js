68ea866626d40b6d769114d5f018341d27fc599d
Luigi Asprino <luigi.asprino@gmail.com>
2021-01-02 13:30:50 +0100

    map resize

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 var svgObject;
 var svgElem;
-
 var loadBoundingClientRectSvg;
 
 window.addEventListener("load", function() {
   svgObject = document.getElementById('svg-object').contentDocument;
   svgElem = svgObject.getElementsByTagName("svg")[0];
-	//svgElem.setAttribute("width", "100%");
-	//svgElem.setAttribute("height", "100%");
 	svgElem.setAttribute("viewBox","0 0 1280 680");
-	//svgElem.setAttribute("preserveAspectRatio","none");
-
-
 	loadBoundingClientRectSvg = svgElem.getBoundingClientRect();
 	svgElem.removeAttribute("enable-background");
 	svgElem.removeAttribute("width");
@@ -23,18 +17,7 @@ window.addEventListener("load", function() {
   var svg = svgObject.getElementsByClassName("RequirementCollection");
   for (var i = 0; i < svg.length; i++) {
     svg[i].style.cursor = "pointer";
-    svg[i].addEventListener("click", function() {
-			if(svgElem.getAttribute("transform")){
-				svgElem.removeAttribute("transform");
-			} else 	{
-				var currentWidth = svgElem.getBoundingClientRect().width;
-				var currentHeight = svgElem.getBoundingClientRect().height ;
-				var calcWidth = currentWidth/5;
-				var calcHeight = currentHeight/5;
-				svgElem.setAttribute("transform", "translate ("+(2*calcWidth) + " -"+(2*calcHeight)+ ") scale(0.2)");
-			}
-
-    });
+    svg[i].addEventListener("click", clickTask);
     svg[i].addEventListener("mouseenter", function(event) {
       elementMouseEnter("RequirementCollection");
     }, false);
@@ -57,4 +40,28 @@ function elementMouseEnter(clazz) {
 
 function elementMouseLeave(clazz) {
   setAttribute(clazz, "opacity", "1.0");
+}
+
+var doClickSvg;
+
+function clickTask(e) {
+	if(svgElem.getAttribute("transform") == undefined){
+		var currentWidth = svgElem.getBoundingClientRect().width;
+		var currentHeight = svgElem.getBoundingClientRect().height ;
+		var calcWidth = currentWidth/5;
+		var calcHeight = currentHeight/5;
+		svgElem.setAttribute("transform", "translate ("+(2*calcWidth) + " -"+(2*calcHeight)+ ") scale(0.2)");
+		doClick = (event) => resetSvg(event, e);
+		svgElem.addEventListener('click', doClickSvg);
+		svgElem.style.cursor = "pointer";
+
+	}
+}
+
+function resetSvg(event1, e) {
+	if(event1!=e){
+		svgElem.removeAttribute("transform");
+		svgElem.removeEventListener('click', doClickSvg);
+		svgElem.style.cursor = "";
+	}
 }
