faa31e543128f6002d6420a8f973dba27ed48868
Luigi Asprino <luigi.asprino@gmail.com>
2021-01-04 23:58:18 +0100

    logo enlarge button json

--- js/script.js
+++ js/script.js
@@ -1,67 +1,66 @@
-var svgObject;
-var svgElem;
-var loadBoundingClientRectSvg;
+var enlarge = "<span class=\"dot\">	<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-arrows-angle-expand\" viewBox=\"0 0 16 16\">		<path fill=\"white\" fill-rule=\"evenodd\" d=\"M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z\" />	</svg></span>";
+
 
 window.addEventListener("load", function() {
-  svgObject = document.getElementById('svg-object').contentDocument;
-  svgElem = svgObject.getElementsByTagName("svg")[0];
-	svgElem.setAttribute("viewBox","0 0 1280 680");
-	loadBoundingClientRectSvg = svgElem.getBoundingClientRect();
-	svgElem.removeAttribute("enable-background");
-	svgElem.removeAttribute("width");
-	svgElem.removeAttribute("height");
-	svgElem.removeAttribute("x");
-	svgElem.removeAttribute("y");
-
-
-  var svg = svgObject.getElementsByClassName("RequirementCollection");
-  for (var i = 0; i < svg.length; i++) {
-    svg[i].style.cursor = "pointer";
-    svg[i].addEventListener("click", clickTask);
-    svg[i].addEventListener("mouseenter", function(event) {
-      elementMouseEnter("RequirementCollection");
-    }, false);
-    svg[i].addEventListener("mouseleave", function(event) {
-      elementMouseLeave("RequirementCollection");
-    }, false);
-  }
+  loadSVG("img/mappa.svg", $("#svg-object"), "0 0 1280 680", mapCallBack);
+  //loadSVG("img/mappa.svg", $("#svg-min"), "0 0 1280 680");
+  //loadSVG("img/mappa.svg", $("#svg-logo"), "0 0 1280 680");
 });
 
-function setAttribute(clazz, attribute, value) {
-  var svg2 = svgObject.getElementsByClassName(clazz);
-  for (var i = 0; i < svg2.length; i++) {
-    svg2[i].setAttribute(attribute, value);
+function mapCallBack(p) {
+  $.getJSON("interaction.json", function(data) {
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        makeClassActionable(key);
+        setAction(key, data[key]);
+      }
+    }
+  });
+}
+
+function makeClassActionable(clazz) {
+  $("." + clazz).css("cursor", "pointer");
+  $("." + clazz).on("mouseenter", function() {
+    $("." + clazz).attr("opacity", "0.8");
+  });
+  $("." + clazz).on("mouseleave", function() {
+    $("." + clazz).attr("opacity", "1.0");
+  });
+}
+
+function setAction(clazz, action) {
+  for (var e in action) {
+    if (action[e]["type"] == "svg") {
+      $("." + clazz).on(e, function() {
+        if ($("#svg-min svg").length > 0) {
+          $("#svg-object svg").remove();
+        } else {
+          $("#svg-object svg").appendTo("#svg-min");
+					$("#svg-min").parent().append(enlarge);
+					$(".dot").css("cursor","pointer");
+					$(".dot").on("click",function(){
+						$("#svg-object svg").remove();
+						$(".dot").remove();
+						$("#svg-min svg").appendTo("#svg-object");
+					});
+        }
+        loadSVG(action[e]["path"], $("#svg-object"), action[e]["viewBox"], mapCallBack);
+      });
+    }
   }
 }
 
-function elementMouseEnter(clazz) {
-  setAttribute(clazz, "opacity", "0.8");
-}
-
-function elementMouseLeave(clazz) {
-  setAttribute(clazz, "opacity", "1.0");
-}
-
-var doClickSvg;
-
-function clickTask(e) {
-	if(svgElem.getAttribute("transform") == undefined){
-		var currentWidth = svgElem.getBoundingClientRect().width;
-		var currentHeight = svgElem.getBoundingClientRect().height ;
-		var calcWidth = currentWidth/5;
-		var calcHeight = currentHeight/5;
-		svgElem.setAttribute("transform", "translate ("+(2*calcWidth) + " -"+(2*calcHeight)+ ") scale(0.2)");
-		doClick = (event) => resetSvg(event, e);
-		svgElem.addEventListener('click', doClickSvg);
-		svgElem.style.cursor = "pointer";
-
-	}
-}
-
-function resetSvg(event1, e) {
-	if(event1!=e){
-		svgElem.removeAttribute("transform");
-		svgElem.removeEventListener('click', doClickSvg);
-		svgElem.style.cursor = "";
-	}
+function loadSVG(file, elem, viewBox, callback) {
+  elem.load(file, function() {
+    var svg = elem.find("svg");
+    svg.removeAttr("enable-background");
+    svg.removeAttr("width");
+    svg.removeAttr("height");
+    svg.removeAttr("x");
+    svg.removeAttr("y");
+    svg.attr("viewBox", viewBox);
+    svg.attr("height", "100%");
+    svg.attr("width", "100%");
+    callback("BLA");
+  });
 }
